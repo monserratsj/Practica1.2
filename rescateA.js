8ae1fa8725c675f8f1d063143cdd98fcbbad8fa4
Mon <monsesuju99@gmail.com>
2024-03-13 10:19:21 -0600

    posible codigo para primer punto a revisar

--- rescateA.js
+++ rescateA.js
@@ -1,1 +1,32 @@
 //Aqui va el codigo del primer problema
+function accionesDisponibles(prisioneroDespierto, caballeroDurmiendo, arqueroDurmiendo, perroPresente) {
+    const acciones = [];
+
+    if (caballeroDurmiendo && perroPresente) {
+        acciones.push('Ataque rápido');
+    }
+
+    if (!caballeroDurmiendo || !arqueroDurmiendo) {
+        acciones.push('Espía');
+    }
+
+    if (prisioneroDespierto && arqueroDurmiendo) {
+        acciones.push('Señalar al prisionero');
+    }
+
+    if (perroPresente) {
+        if (prisioneroDespierto && caballeroDurmiendo && arqueroDurmiendo) {
+            acciones.push('Prisionero libre');
+        }
+    } else {
+        if (prisioneroDespierto && !caballeroDurmiendo && !arqueroDurmiendo) {
+            acciones.push('Prisionero libre');
+        }
+    }
+
+    return acciones;
+}
+
+// Ejemplo de uso
+const accionesDisponiblesParaAnnalyn = accionesDisponibles(true, false, true, true);
+console.log("Acciones disponibles para Annalyn:", accionesDisponiblesParaAnnalyn);
